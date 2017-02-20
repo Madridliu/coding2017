443169f25057fe2490d55eb6a8c470b74284c3aa
shane <baohua.shan@zhulebei.com>
2017-02-20 15:24:53 +0800

    初始化数据结构代码

---
+++ group16/214074094/src/com/coding/basic/ArrayList.java
@@ -0,0 +1,34 @@
+package com.coding.basic;
+
+public class ArrayList implements List {
+	
+	private int size = 0;
+
+    private static int DEFAULT_CAPACITY = 10;
+	
+	private Object[] elementData = new Object[DEFAULT_CAPACITY];
+	
+	public void add(Object o){
+		
+	}
+	public void add(int index, Object o){
+
+	}
+	
+	public Object get(int index){
+		return null;
+	}
+	
+	public Object remove(int index){
+		return null;
+	}
+	
+	public int size(){
+		return -1;
+	}
+	
+	public Iterator iterator(){
+		return null;
+	}
+	
+}
